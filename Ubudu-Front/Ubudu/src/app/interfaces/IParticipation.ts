c2e0f6db117cc76aaecbfe086b408869b916b915
Hawkker <thomas.castro@epitech.eu>
2020-09-03 14:41:12 +0200

    Remove dup dir

--- Ubudu-Front/Ubudu/src/app/interfaces/IParticipation.ts
+++
@@ -1,4 +0,0 @@
-export interface IParticipation {
-    answers: string[];
-    score: number;
-}
--- Ubudu-Front/Ubudu/src/app/interfaces/IUser.ts
+++
@@ -1,8 +0,0 @@
-import { IParticipation } from './IParticipation';
-
-export interface IUser {
-    _id: string;
-    username: string;
-    participation: IParticipation[];
-    token: string;
-}
